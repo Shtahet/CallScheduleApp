42c015a4c2e6b0c0f90335c6d90e623aa0a9aa04
Alexandr Gergalo <derien001@gmal.com>
2018-06-26 23:39:09 +0300

    Add necessary property to Day class. Realize INotifyPropertyChange

--- CallScheduleApp/Day.cs
+++ CallScheduleApp/Day.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CallScheduleApp
 {
-    class Day
+    class Day:INotifyPropertyChanged
     {
         private DateTime _d;
+        private bool _isWorkDay;
+        private bool _isCurrDay;
 
         public Day(DateTime date)
         {
@@ -17,5 +21,38 @@ namespace CallScheduleApp
 
         public int NumDay => _d.Day;
 
+        public bool IsWorkingDay
+        {
+            get { return _isWorkDay; }
+            set
+            {
+                _isWorkDay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsCurrentDay
+        {
+            get { return _isCurrDay; }
+            set
+            {
+                _isCurrDay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime Date {
+            get { return _d; }
+            set { _d = value;
+                OnPropertyChanged("NumDay");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
