0625487ad712ce4d5021fa63151731d8f4cab4c6
Alexandr Gergalo <derien001@gmal.com>
2018-06-26 22:52:08 +0300

    Add class witch responsible for days with property

---
+++ CallScheduleApp/Day.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallScheduleApp
+{
+    class Day
+    {
+        private DateTime _d;
+
+        public Day(DateTime date)
+        {
+            _d = date;
+        }
+
+        public int NumDay => _d.Day;
+
+    }
+}
